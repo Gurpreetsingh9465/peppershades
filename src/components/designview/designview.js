4ff7f54fb6869515d7b03c7c9a670d04b08b2390
shivali258 <shivalisharmass123@gmail.com>
2019-10-12 13:11:21 +0530

    select and addd

---
+++ src/components/designview/designview.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import template from './designview.html'
+import styles from './designview.less'
+
+export default Vue.extend({
+  template: template,
+  data: function () {
+    return {}
+  },
+  methods: {},
+  created: function () {
+  }
+});
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import ProjectForm from '../components/projectform/projectform';
 import marketBusiness from '../components/marketbusiness/marketbusiness';
 import chooseService from '../components/chooseService/chooseService';
 import homepage from '../components/homepage/homepage'
+import selectandadd from '../components/selectandadd/selectandadd'
+import designview from '../components/designview/designview'
 
 Vue.use(Router);
 export default new Router({
@@ -31,6 +33,16 @@ export default new Router({
           path: '/marketBusiness',
           name: 'marketBusiness',
           component: marketBusiness
+        },
+        {
+          path:'/selectandadd',
+          name: 'selectandadd',
+          component:selectandadd
+        },
+        {
+          path:'/designview',
+          name:'designview',
+          component:designview
         },
         {
           path: '/chooseService',

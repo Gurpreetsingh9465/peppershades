f25c6234ea2bb56209678bef98cc763930d0b2b7
shivali258 <shivalisharmass123@gmail.com>
2019-12-23 03:55:00 +0530

    validations

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,24 +1,46 @@
-/* eslint-disable */
 
-import Vue from 'vue';
-import template from './signup.html'
-import styles from './signup.less'
-import socialBar from '../UI/socialBar.vue'
+            import Vue from 'vue';
+            import template from './signup.html'
+            import styles from './signup.less'
+            import socialBar from '../UI/socialBar.vue'
+            import { ValidationObserver, extend, localize } from 'vee-validate';
+ 
+            import { ValidationProvider } from 'vee-validate/dist/vee-validate.full.esm';
+
+
+          // Add a rule.
+extend('secret', {
+  validate: value => value === 'example',
+  message: 'This is not the magic word'
+});
+
+// Register it globally
+Vue.component('ValidationProvider', ValidationProvider);
+          
 
 export default Vue.extend({
+  
     template: template,
     components: {
-            socialBar
+            socialBar,
+         
         },
-
-    data: function() {
-        
-        return {
-
-        }
-    },
-    methods: {
-
-    },
-    created: function() {}
+  data: function () {
+    return {
+        user: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: ''
+        },
+        submitted: false
+    }
+  },
+  methods: {
+    onSubmit () {
+      alert('Form has been submitted!');
+    }
+  },
+  created: function () {
+  }
 });
--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue';
 import Router from './router';
 import Root from './components/root/root';
+import { ValidationObserver, ValidationProvider, extend, localize } from 'vee-validate';
+import en from 'vee-validate/dist/locale/en.json';
+import * as rules from 'vee-validate/dist/rules';
+
+// install rules and localization
+Object.keys(rules).forEach(rule => {
+  extend(rule, rules[rule]);
+});
+
+localize('en', en);
+
+// Install components globally
+Vue.component('ValidationObserver', ValidationObserver);
+Vue.component('ValidationProvider', ValidationProvider);
 
 window.idpApp = new Vue({
   el: '#app',
